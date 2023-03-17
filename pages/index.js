08752766b5fe35d9fcb52f0e180679ecf5050218
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-03-17 17:10:35 +0100

    test deployment

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import ImageBoard from "@/components/Collage";
 import LandingPage from "@/components/LandingPage";
 import LoginForm from "@/components/LoginForm";
-import Login from "@/components/GoogleLogin";
 
 export default function Start() {
   return (
