1d9d9e50fb464b1ca97ddee8d50fce54a7a97f24
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-03-21 17:16:28 +0100

    login with google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,53 +1,17 @@
-import CredentialProvider from "next-auth/provides/credentials"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
-import dbConnect from "@/db/connect";
-import User from "@/db/models/User";
-
-import { verifyPassword } from "@/db/models/utils";
 
 export const authOptions = {
   session: {
     jwt: true,
   },
-
   providers: [
-    CredentialProvider({
-      name: "credentials",
-      credentials: {
-        name: { label: "name", type: "text" },
-        email: { label: "email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-
-      async authorize(credentials) {
-        await dbConnect();
-        const user = await User.findOne({ email: credentials.email });
-
-        if (!user) {
-          throw new Error("No user found!");
-        }
-
-        const isValid = await verifyPassword(
-          credentials.password,
-          user.password
-        );
-
-        if (!isValid) {
-          throw new Error('Could not log you in!');
-        }
-
-        return { name: user.name, email: user.email };
-      },
-    }),
-    // ...add more providers here_____________________________
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.JWT_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
-export default NextAuth(authOptions)
-
+export default NextAuth(authOptions);
