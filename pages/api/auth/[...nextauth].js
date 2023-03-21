7c14e77949630b1a709ccad0d4e526142c4a084e
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-03-21 17:04:33 +0100

    test login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
 import CredentialProvider from "next-auth/provides/credentials"
 import GoogleProvider from "next-auth/providers/google";
 
